73852d6406758390cab4d0fa9b18505cf213e541
tropikbrice <blabejof@gmail.com>
2017-10-17 16:31:57 +0200

    amelioration class le scraping mairie

--- S03_ruby_intermediaire/J2_scraping.rb
+++ S03_ruby_intermediaire/J2_scraping.rb
@@ -10,14 +10,16 @@ require 'nokogiri'
 require 'open-uri'
 require "google_drive"
 require "pry"
-require_relative "/home/brice/Documents/THP/S02J04/rte_mairie.rb"
 
-
-class scraping
+class Scraping
 	#class de fonctions permettant de realiser du scraping des mairies de france
 	#via l'url
 
 	attr_accessor :page_url
+
+	def initialize(page_url)
+		@page_url = "http://annuaire-des-mairies.com/creuse.html"
+	end
 
 	#récupère l'adresse email à partir de l'url d'une mairie
 	#pour plus de details sur la construction des fonctions :
@@ -72,6 +74,36 @@ class scraping
 	end
 
 
+	# #enregistrement dans un spreadsheet
+	# def login_drive(session)
+	# 	@session ||= GoogleDrive::Session.from_config("client_secret.json")
+	# end
 
+	# # #tab est un tableau car la fonction de scrapp retourne un tableau de hashs
+	# #tab= get_email_hash()
+	# def get_the_name_and_email_and_put_it_in_spreadsheet(fichier_mairie, tab)
+	# 	#je me positionne dans le 1er onglet de mon fichier spreadsheet drive (excel drive)
+	# 	onglet1 = fichier_mairie.worksheets[0]
+
+	# 	# on demarre
+	# 	# i à 2 car la 1ere ligne est la ligne qui contient les noms de colonne , i va de 2 à taille du tableau de hash soit 185+2=187
+	# 	i=2
+	# 	#binding.pry
+	# 	tab.each do |element_du_tab_est_hash|
+	# 		onglet1[i,1] = element_du_tab_est_hash[:name]
+
+	# 		onglet1[i,2] = element_du_tab_est_hash[:email]
+
+	# 		#incrementation des lignes "i" en dehors des boucles d'insertion des valeurs
+	# 		i += 1
+	# 	end
+
+	# 	onglet1.save
+	# end
 
 end
+
+#execution du programme
+
+scrap23 = scraping.new
+
