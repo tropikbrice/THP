6eae6b4533d9a709589cebf4485714f9a92e6047
tropikbrice <blabejof@gmail.com>
2017-11-09 18:09:58 +0100

    twitter is working !

--- S06_rails_avance/S06J4/twitter-bis/app/services/send_twitter.rb
+++ S06_rails_avance/S06J4/twitter-bis/app/services/send_twitter.rb
@@ -4,18 +4,21 @@ class SendTwitter
 
 	def initialize(tweet_string)
 		@tweet_string = tweet_string
+		log_in_to_twitter
+		send_tweet
 	end
 
 	def log_in_to_twitter
-		@client_rest = Twitter::REST::Client.new do |config|
-			config.consumer_key        = ENV["cons_key"] # "YOUR_CONSUMER_KEY"
-			config.consumer_secret     = ENV["cons_secret"] #{}"YOUR_CONSUMER_SECRET"
-			config.access_token        = ENV["acc_token"] #{}"YOUR_ACCESS_TOKEN"
-			config.access_token_secret = ENV["acc_token_secret"] #{}"YOUR_ACCESS_SECRET"	
+		@client = Twitter::REST::Client.new do |config|
+			config.consumer_key        = Figaro.env.cons_key # "YOUR_CONSUMER_KEY"
+			config.consumer_secret     = Figaro.env.cons_secret #{}"YOUR_CONSUMER_SECRET"
+			config.access_token        = Figaro.env.acc_token #{}"YOUR_ACCESS_TOKEN"
+			config.access_token_secret = Figaro.env.acc_token_secret #{}"YOUR_ACCESS_SECRET"
+		end
 	end
 
 	def send_tweet
-		@client.update('@tweet_string')
+		@client.update( @tweet_string )
 	end
 
 end
