6fb7e73a6a5c428b4bccac7b91e89eb2a9359f59
tropikbrice <blabejof@gmail.com>
2017-11-15 20:49:55 +0100

    S07J4 projet landings pages a gogo initial commit

---
+++ S07_html_css/S07J4_landingPages/landing-pages/app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+	layout false
+	
+	def checkr
+	end
+
+	def flynn
+	end
+
+	def sparks
+	end
+end
---
+++ S07_html_css/S07J4_landingPages/landing-pages/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+	get 'home/checkr'
+
+	get 'home/flynn'
+
+	get 'home/sparks'
+end
