622b96125322f7b9604c91ae7f9ea21edd2e7e37
tropikbrice <blabejof@gmail.com>
2017-11-23 18:16:19 +0100

    S08J4 jQuery carousel initial commit

--- S08_html_css_js/S08J3_JS/calc.js
+++ S08_html_css_js/S08J3_JS/calc.js
@@ -12,15 +12,31 @@ function calculator() {
 		let key = keyz[i];
 		if (key !="=") {
 		};
-		tab.push(key.innerText);
-		let calculous = function(){
-			console.log(key.innerText);
-				// console.log( parseInt(key.innerText,10) ) ;
-			};
 
+		let calculous = function(){
+			console.log(this);
 			console.log(tab);
-	//on ecoute les "keys" de la calculatrice
-	key.addEventListener('click',calculous);
-}
+
+			keyz[i].addEventListener('click',calculous); //on ecoute les "keys" de la calculatrice
+		}
+	};
 };
 calculator();
+
+
+// tab.push(key.innerText);
+
+	// var tab = [];
+	// for(let i=0; i< keyz.length; i++) {
+	// 	let key = keyz[i];
+		
+	// 	let calculous = function(){
+	// 		console.log(key.innerText);
+	// 		tab.push(key.innerText);
+	// 	// console.log( parseInt(key.innerText,10) ) ;
+	// };
+
+	// 	//on ecoute les "keys" de la calculatrice
+	// keyz.addEventListener('click',function(){
+	// 	console.log(this.innerText);
+	// });
