b1e236d65d9eea38ee7e7115f7a02382ff19eed7
tropikbrice <blabejof@gmail.com>
2017-11-28 21:10:24 +0100

    S09J2 : affichage maps mais marche pas

--- S09_JS_front/S09J2/api-gmaps/app/controllers/maps_controller.rb
+++ S09_JS_front/S09J2/api-gmaps/app/controllers/maps_controller.rb
@@ -1,4 +1,28 @@
 class MapsController < ApplicationController
-  def index
-  end
+	def index
+		# @map = Map.new
+
+		# return unless param_maps[:map] && param_maps[:map][:latitude] && param_maps[:map][:longitude] && param_maps[:map][:message]
+		# @map.latitude = param_maps[:map][:latitude]
+		# @map.longitude = param_maps[:map][:longitude]
+		# @map.message = param_maps[:map][:message]
+
+		# unless @map.valid?
+		# 	@map.errors.full_messages.each do |error|
+		# 		flash.now[:danger] = error
+		# 	end
+		# end
+	end
+
+	def default_map
+		@map.latitude = 45 # default value
+		@map.longitude = 0 # default value
+		@map.message = 'default marker' # default value
+	end
+
+	def param_maps
+		# [:latitude, :longitude] équivaut à %i[latitude longitude]
+		params.permit(map: %i[latitude longitude message])
+	end
 end
+
--- S09_JS_front/S09J2/instructions.rb
+++ S09_JS_front/S09J2/instructions.rb
@@ -1,3 +1,63 @@
+# on ajoute gem figaro pour gerer les key caches dans gemfile
+# on installe figaro
+bundle exec figaro install
+# => creation de fichier config/applications.yml et .gitignore
+
+# Pour le deploiement sur Heroku :
+# https://github.com/laserlemon/figaro
+# Heroku
+# Heroku already makes setting application configuration easy:
+
+# $ heroku config:set google_analytics_key=UA-35722661-5
+# Using the figaro command, you can set values from your configuration file all at once:
+
+# $ figaro heroku:set -e production
+# For more information:
+
+# $ figaro help heroku:set
+
+
 # apres bundle update :
 # on genere un controlleur avec une methode index afin d'afficher la page avec gmaps
 rails g controller Maps index
+
+# configuration des routes
+# => config/routes.rb
+
+##########
+# on s'attaque aux view now
+
+# parametrage des api_key de google maps en mode secret via secrets.yml
+
+# on copie le script d'appel à l'API dans /maps/index.html.erb
+# puis on l'appelle via :
+	# <script src="https://maps.googleapis.com/maps/api/js?key=<%=Figaro.env.google_api_key%> &callback=initMap" async defer></script>
+# directement dans le code html a la fin du code
+
+##############
+# on cree la partie html du formulaire : cf code dans index.html.erb
+
+########## JS
+# on va faire du js dans un fichier separe pr que ça soit propre le html.erb
+# => on cree un fichier : ATTENTION , il faut creer dossier nomController !!!!
+/app/assets/javascripts/maps/maps.js
+# on va tout de suite l ajouter à rails pour qu'il le precompile dans le fichier
+/config/initializers/assets.rb
+
+# on copie le bout de code pris sur site google maps : https://developers.google.com/maps/documentation/javascript/examples/map-simple?hl=fr
+#       var map;
+#       function initMap() {
+#         map = new google.maps.Map(document.getElementById('map'), {
+#           center: {lat: -34.397, lng: 150.644},
+#           zoom: 8
+#         });
+#       }
+# }
+
+# on copie 
+#     <div id="map"></div>
+# dans la view index.html
+
+bon j'ai un peu saute bcp de choses, en gros :
+Asset was not declared to be precompiled in production.
+Add `Rails.application.config.assets.precompile += %w( maps.js )` to `config/initializers/assets.rb` and restart your server
